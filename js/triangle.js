72fa68aa4b5bc5091792c16a6472d90aba8dc3dd
Tomasz <tomaw@wp.pl>
2016-12-25 13:24:34 +0100

    adding task from module 8.4

---
+++ js/triangle.js
@@ -0,0 +1,7 @@
+// plik triangle.js
+
+var a = prompt ('Wpisz długość boku trójkąta');
+var h = prompt ('Wpisz długość wysokości trójkąta')
+var triangleArea = a*h/2;
+alert('Witaj, pole trójkąta to ' + triangleArea);
+console.log('Witaj, pole trójkąta to' + triangleArea);
