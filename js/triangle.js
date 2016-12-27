a57a0cfb15a7855d74164ad4de92b521559dc769
Tomasz <tomaw@wp.pl>
2016-12-27 13:45:16 +0100

    usuniecie

--- js/triangle.js
+++
@@ -1,7 +0,0 @@
-// plik triangle.js
-
-var a = prompt ('Wpisz długość boku trójkąta');
-var h = prompt ('Wpisz długość wysokości trójkąta')
-var triangleArea = a*h/2;
-alert('Witaj, pole trójkąta to ' + triangleArea);
-console.log('Witaj, pole trójkąta to' + triangleArea);
