19c12697148f1e4b2623d1a4676d70470009c6b2
Tomasz <tomaw@wp.pl>
2016-12-29 11:16:48 +0100

    zmiany

--- funkcje/funkcje.js
+++ funkcje/funkcje.js
@@ -1,13 +1,15 @@
-var a  = '2',
-    h  = '4';
+// plik funkcje.js
+
 var triangle1Area = getTriangleArea(10, 15);
-var triangle2Area = getTriangleArea(12, 18);
-var triangle3Area = getTriangleArea(15, 21);
+	triangle2Area = getTriangleArea(12, 18);
+	triangle3Area = getTriangleArea(15, 21);
+
 function getTriangleArea(a, h) {
 
-	if ( (a <= 0) && (h <= 0) ) {
+	if ( (a <= 0) || (h <= 0) ) {
+		
 		console.log('Nieprawidłowe dane')
 }
 	return (a*h/2);
 }
-	console.log( getTriangleArea(10,6) )
+		console.log( getTriangleArea(10,6) )
