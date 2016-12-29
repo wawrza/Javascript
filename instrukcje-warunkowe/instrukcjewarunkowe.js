0afb5fd142d757ab709da7a3b816bc772feac9cb
Tomasz <tomaw@wp.pl>
2016-12-29 11:06:33 +0100

    zmiany

--- instrukcje-warunkowe/instrukcjewarunkowe.js
+++ instrukcje-warunkowe/instrukcjewarunkowe.js
@@ -1,7 +1,7 @@
 // plik instrukcjewarunkowe.js
 
 var a = 2;
-var b = 3;
+	b = 3;
 var value = (a*a) + (2 * a * b) - (b*b);
 if (value > 0) {
 
@@ -12,11 +12,7 @@ else if (value < 0) {
 
 	console.log("wynik ujemny");
 }
-if (value == 0) {
+else (value == 0) {
 
 	console.log ("wynik jest równy zero")
-}
-else if ((value > 0) || (value < 0)) {
-
-	console.log ('wynik nie jest równy zero')
 }
