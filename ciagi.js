2c387dd63968e6966e5491524c1399af06fbdc90
Tomasz <tomaw@wp.pl>
2016-12-30 17:31:54 +0100

    dodanie

--- ciagi.js
+++ ciagi.js
@@ -1,15 +1,15 @@
 // plik ciagi.js
 
 var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.",
-	text2 = "Zielone słonie",
-	text2UpperCased = text2.toUpperCase(),
-	animal = text.slice(0,6),
-	textChanged = text.replace("Papugi", "ZIELONE SŁONIE"),
-	textChangedAfter = textChanged.substr(0,45);
+    text2 = "Zielone słonie",
+    text2UpperCased = text2.toUpperCase(),
+    animal = text.slice(0,6),
+    textChanged = text.replace("Papugi", "ZIELONE SŁONIE"),
+    textChangedAfter = textChanged.substr(0,45);
 
 
-	console.log(text2UpperCased);
-	console.log(animal);
-	console.log(textChanged.length);
-	console.log(textChanged);
-	console.log(textChangedAfter);
+    console.log(text2UpperCased);
+    console.log(animal);
+    console.log(textChanged.length);
+    console.log(textChanged);
+    console.log(textChangedAfter);
