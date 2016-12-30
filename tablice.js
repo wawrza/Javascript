44dc6e2a039f98d15df57cfc71aad5ec9016c3e9
Tomasz <tomaw@wp.pl>
2016-12-30 11:37:53 +0100

    poprawki

--- tablice.js
+++ tablice.js
@@ -1,11 +1,11 @@
 // plik tablice.js
 
 var womenNames = ['Asia', 'Kasia', 'Ola', 'Jola'],
-	menNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'],
-	allNames = womenNames.concat(menNames),
-	newName ='Marian';
+    menNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'],
+    allNames = womenNames.concat(menNames),
+    newName ='Marian';
 
 if (allNames.indexOf(newName) == -1) {
-	var newName = allNames.push('Marian');
+    var newName = allNames.push('Marian');
 }
-	console.log(allNames)
+    console.log(allNames)
