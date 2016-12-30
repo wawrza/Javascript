cdef32617dff61d380e6fd81969b28ab8a6caee7
Tomasz <tomaw@wp.pl>
2016-12-30 10:31:27 +0100

    poprawki

---
+++ instrukcjewarunkowe.js
@@ -0,0 +1,14 @@
+// plik instrukcjewarunkowe.js
+
+var a = 2,
+	b = 3,
+	value = (a*a) + (2 * a * b) - (b*b);
+
+if (value > 0) {
+    console.log("wynik dodatni");
+} else if (value < 0) {
+    console.log("wynik ujemny");
+} else {
+    console.log ("wynik jest równy zero");
+}
+
