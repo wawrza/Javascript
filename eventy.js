773019f96065c90534e29fb4a4edb6eea03f4dc2
Tomasz <tomaw@wp.pl>
2017-01-02 00:36:13 +0100

    dodanie

---
+++ eventy.js
@@ -0,0 +1,11 @@
+// plik eventy.js
+
+var list = document.getElementById('Lista'),
+	add = document.getElementById('addElem'),
+	newElem = document.createElement('li');
+
+	add.addEventListener('click', function plus(plus) {
+		newElem.innerHTML += '<li>item</li>';
+		list.appendChild(newElem);
+			console.log(plus);
+});
