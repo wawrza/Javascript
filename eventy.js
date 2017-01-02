34bd457d74695cae003834fb3455c466caae0031
Tomasz <tomaw@wp.pl>
2017-01-02 10:16:50 +0100

    dodanie

--- eventy.js
+++ eventy.js
@@ -3,8 +3,10 @@
 var list = document.getElementById('Lista'),
     add = document.getElementById('addElem'),
     newElem = document.createElement('li');
-
+    index = document.getElementsByTagName('li');
+    
     add.addEventListener('click', function plus(plus) {
-        newElem.innerHTML += '<li>item</li>';
+        newElem.innerHTML += '<li>item</li>' + [index.length];
         list.appendChild(newElem);
 });
+    console.log(index.length);
