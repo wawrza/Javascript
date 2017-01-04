bde205c0c323b3a6917ed583766a2ed806840c82
Tomasz <tomaw@wp.pl>
2017-01-04 10:26:22 +0100

    dodanie

---
+++ petle.js
@@ -0,0 +1,19 @@
+// plik petle.js
+
+function rysujChoinke(row) {
+	
+	for (var i = 1; i <= row; i++) {
+	var star = "";
+  	for (var j = 0; j < i*2-1; j++) {
+    star += '*';
+    }
+   	console.log(star);
+    }
+}
+
+rysujChoinke(1);
+
+rysujChoinke(2)
+
+rysujChoinke(5);
+
