37e9671d3725d0e7f629689d91cbf996de318e4f
Tomasz <tomaw@wp.pl>
2017-01-10 09:16:14 +0100

    zmiany

--- telefony.js
+++ telefony.js
@@ -6,14 +6,13 @@ function Telefon(marka, cena, kolor) {
 	this.kolor = kolor;
 }
 	Telefon.prototype.printInfo = function() {
-
 	console.log("Marka telefonu to " + this.marka + ", kolor to " + this.kolor + ", a cena to " + this.cena + ".");	
 }
-var SamsungGalaxyS6 = new Telefon("Samsung", 3199, "złoty");
-var OnePlusOne = new Telefon("OnePlus", 1749, "biały");
-var iPhone6S = new Telefon("Apple", 2250, "srebrny");
+	var SamsungGalaxyS6 = new Telefon("Samsung", 3199, "złoty");
+	var OnePlusOne = new Telefon("OnePlus", 1749, "biały");
+	var iPhone6S = new Telefon("Apple", 2250, "srebrny");
 
-iPhone6S.printInfo();
-SamsungGalaxyS6.printInfo();
-OnePlusOne.printInfo();
+	iPhone6S.printInfo();
+	SamsungGalaxyS6.printInfo();
+	OnePlusOne.printInfo();
 
