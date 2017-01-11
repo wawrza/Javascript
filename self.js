7cb0be3b722fe93914db52546e1a52aae628dfa0
Tomasz <tomaw@wp.pl>
2017-01-11 16:52:22 +0100

    dodanie

---
+++ self.js
@@ -0,0 +1,15 @@
+function Button(text) {
+	this.text = text || 'Hello';
+}
+Button.prototype = {
+	var self = this;
+	this.$element = $('<button>');
+	this.$element.text(this.text);
+	this.$element.click(function() {
+		alert(self.text);
+		$(‘body’).append(this.$element);
+		});
+	}
+}
+	var btn1 = new Button('Hello!');
+	btn1.create(); 
